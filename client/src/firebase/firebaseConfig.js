495c0c453dfef53d28c7965afd3ff1ebd14b0b9b
harshkg2001 <harshvsharsh2001@gmail.com>
2024-06-25 14:33:19 +0530

    removed sensitivity :)

--- client/src/firebase/firebaseConfig.js
+++ client/src/firebase/firebaseConfig.js
@@ -4,12 +4,12 @@ import {getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBgJHu0_pGG7dMFbVsdzkaAIQ5bnhrlzoA",
-  authDomain: "zcoders-80485.firebaseapp.com",
-  projectId: "zcoders-80485",
-  storageBucket: "zcoders-80485.appspot.com",
-  messagingSenderId: "882570050734",
-  appId: "1:882570050734:web:171c4f199c83388229413f"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 // Initialize Firebase
--- client/src/pages/AllProblems.jsx
+++ client/src/pages/AllProblems.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AllProblems.scss'
 import { Link } from'react-router-dom'
+import data from '../data.json'
 
 function AllProblems()
 {
+  const [currPage, setCurrPage] = useState(1);
+  const [postPerPage, setPostPerPage] = useState(5);
+  const lastIndex = currPage*postPerPage;
+  const firstIndex = lastIndex-postPerPage;
+  const maxPage = Math.ceil(data.length/postPerPage);
+  const arr=data.slice(firstIndex, lastIndex);
+
+  function handleChange(e)
+  {
+    console.log(e.target.value);
+    setPostPerPage(e.target.value);
+    setCurrPage(1);
+  }
+
   return (
     <div className='all-problems'>
-      AllProblems
+      <div className="heading">
+        <h1>All Problems</h1>
+      </div>
+      {
+        arr.map((problem, idx) => (
+          <div key={idx} className='problem-card'>
+            <p className='problem-name'>
+              {problem.id}. {problem.name}
+            </p>
+          </div>
+        ))
+      }
+      <select value={postPerPage} onChange={handleChange} className='select'>
+        <option value="5">5/page</option>
+        <option value="10">10/page</option>
+        <option value="25">25/page</option>
+      </select>
+
+      <div className='pagination'>
+        <button className='button' onClick={() => setCurrPage(Math.max(1, currPage-1))}>Prev</button>
+        <span className='curr-page'>{currPage}/{maxPage}</span>
+        <button className='button' onClick={() => setCurrPage(Math.min(maxPage, currPage+1))}>Next</button>
+      </div>
 
       <Link to='/contribute' className='link'>
         <button className='add'>
